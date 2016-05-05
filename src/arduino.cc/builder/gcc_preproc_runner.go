arduino/arduino-builder

commit 310f2e22b0c96c073adf1b6c1b287db8cef29d96

BuildProperties are now part of Context
Signed-off-by: Cristian Maglie <c.maglie@arduino.cc>

src/arduino.cc/builder/gcc_preproc_runner.go
@@ -108,11 +108,7 @@ func prepareGCCPreprocRecipeProperties(context map[string]interface{}, ctx *type
 		targetFilePath = filepath.Join(preprocPath, targetFilePath)
 	}
 
-	properties := make(props.PropertiesMap)
-	if p, ok := context[constants.CTX_BUILD_PROPERTIES]; ok {
-		properties = p.(props.PropertiesMap).Clone()
-	}
-
+	properties := ctx.BuildProperties
 	properties[constants.BUILD_PROPERTIES_SOURCE_FILE] = sourceFilePath
 	properties[constants.BUILD_PROPERTIES_PREPROCESSED_FILE_PATH] = targetFilePath
 

src/arduino.cc/builder/includes_finder_with_gcc.go
@@ -33,7 +33,6 @@ import (
 	"arduino.cc/builder/builder_utils"
 	"arduino.cc/builder/constants"
 	"arduino.cc/builder/i18n"
-	"arduino.cc/builder/props"
 	"arduino.cc/builder/types"
 	"arduino.cc/builder/utils"
 	"strings"
@@ -44,10 +43,7 @@ type IncludesFinderWithGCC struct {
 }
 
 func (s *IncludesFinderWithGCC) Run(context map[string]interface{}, ctx *types.Context) error {
-	buildProperties := make(props.PropertiesMap)
-	if p, ok := context[constants.CTX_BUILD_PROPERTIES]; ok {
-		buildProperties = p.(props.PropertiesMap).Clone()
-	}
+	buildProperties := ctx.BuildProperties
 	verbose := ctx.Verbose
 	logger := ctx.GetLogger()
 

src/arduino.cc/builder/recipe_runner.go
@@ -33,7 +33,6 @@ import (
 	"arduino.cc/builder/builder_utils"
 	"arduino.cc/builder/constants"
 	"arduino.cc/builder/i18n"
-	"arduino.cc/builder/props"
 	"arduino.cc/builder/types"
 	"os"
 	"sort"
@@ -51,10 +50,7 @@ func (s *RecipeByPrefixSuffixRunner) Run(context map[string]interface{}, ctx *ty
 		logger.Fprintln(os.Stdout, constants.LOG_LEVEL_DEBUG, constants.MSG_LOOKING_FOR_RECIPES, s.Prefix, s.Suffix)
 	}
 
-	buildProperties := make(props.PropertiesMap)
-	if p, ok := context[constants.CTX_BUILD_PROPERTIES]; ok {
-		buildProperties = p.(props.PropertiesMap).Clone()
-	}
+	buildProperties := ctx.BuildProperties
 	verbose := ctx.Verbose
 
 	recipes := findRecipes(buildProperties, s.Prefix, s.Suffix)

src/arduino.cc/builder/add_missing_build_properties_from_parent_platform_txt_files.go
@@ -31,7 +31,6 @@ package builder
 
 import (
 	"arduino.cc/builder/constants"
-	"arduino.cc/builder/props"
 	"arduino.cc/builder/types"
 )
 
@@ -40,13 +39,13 @@ type AddMissingBuildPropertiesFromParentPlatformTxtFiles struct{}
 func (s *AddMissingBuildPropertiesFromParentPlatformTxtFiles) Run(context map[string]interface{}, ctx *types.Context) error {
 	packages := context[constants.CTX_HARDWARE].(*types.Packages)
 	targetPackage := ctx.TargetPackage
-	buildProperties := context[constants.CTX_BUILD_PROPERTIES].(props.PropertiesMap)
+	buildProperties := ctx.BuildProperties
 
 	newBuildProperties := packages.Properties.Clone()
 	newBuildProperties.Merge(targetPackage.Properties)
 	newBuildProperties.Merge(buildProperties)
 
-	context[constants.CTX_BUILD_PROPERTIES] = newBuildProperties
+	ctx.BuildProperties = newBuildProperties
 
 	return nil
 }

src/arduino.cc/builder/coan_runner.go
@@ -32,7 +32,6 @@ package builder
 import (
 	"arduino.cc/builder/constants"
 	"arduino.cc/builder/i18n"
-	"arduino.cc/builder/props"
 	"arduino.cc/builder/types"
 	"arduino.cc/builder/utils"
 	"fmt"
@@ -61,7 +60,7 @@ func (s *CoanRunner) Run(context map[string]interface{}, ctx *types.Context) err
 		return i18n.WrapError(err)
 	}
 
-	buildProperties := context[constants.CTX_BUILD_PROPERTIES].(props.PropertiesMap)
+	buildProperties := ctx.BuildProperties
 	properties := buildProperties.Clone()
 	properties.Merge(buildProperties.SubTree(constants.BUILD_PROPERTIES_TOOLS_KEY).SubTree(constants.COAN))
 	properties[constants.BUILD_PROPERTIES_SOURCE_FILE] = coanTargetFileName

src/arduino.cc/builder/constants/constants.go
@@ -76,7 +76,6 @@ const COAN = "coan"
 const CTAGS = "ctags"
 const CTX_ARCHIVE_FILE_PATH_CORE = "archiveFileCore"
 const CTX_BUILD_CORE = "buildCore"
-const CTX_BUILD_PROPERTIES = "buildProperties"
 const CTX_FILE_PATH_TO_READ = "filePathToRead"
 const CTX_GCC_MINUS_E_SOURCE = "gccMinusESource"
 const CTX_GCC_MINUS_M_OUTPUT = "gccMinusMOutput"

src/arduino.cc/builder/ctags/ctags_runner.go
@@ -32,7 +32,6 @@ package ctags
 import (
 	"arduino.cc/builder/constants"
 	"arduino.cc/builder/i18n"
-	"arduino.cc/builder/props"
 	"arduino.cc/builder/types"
 	"arduino.cc/builder/utils"
 	"fmt"
@@ -41,7 +40,7 @@ import (
 type CTagsRunner struct{}
 
 func (s *CTagsRunner) Run(context map[string]interface{}, ctx *types.Context) error {
-	buildProperties := context[constants.CTX_BUILD_PROPERTIES].(props.PropertiesMap)
+	buildProperties := ctx.BuildProperties
 	ctagsTargetFilePath := ctx.CTagsTargetFile
 	logger := ctx.GetLogger()
 

src/arduino.cc/builder/dump_build_properties.go
@@ -30,8 +30,6 @@
 package builder
 
 import (
-	"arduino.cc/builder/constants"
-	"arduino.cc/builder/props"
 	"arduino.cc/builder/types"
 	"arduino.cc/builder/utils"
 	"fmt"
@@ -41,7 +39,7 @@ import (
 type DumpBuildProperties struct{}
 
 func (s *DumpBuildProperties) Run(context map[string]interface{}, ctx *types.Context) error {
-	buildProperties := context[constants.CTX_BUILD_PROPERTIES].(props.PropertiesMap)
+	buildProperties := ctx.BuildProperties
 
 	keys := utils.KeysOfMapOfString(buildProperties)
 	sort.Strings(keys)

commit 4e5676019fcd09383cda75bbc0a22351382198b1

Fix regression introduced in 310f2e22b0c96c073adf1b6c1b287db8cef29d96
related to #134

Signed-off-by: Cristian Maglie <c.maglie@arduino.cc>

src/arduino.cc/builder/gcc_preproc_runner.go
@@ -108,7 +108,7 @@ func prepareGCCPreprocRecipeProperties(ctx *types.Context, sourceFilePath string
 		targetFilePath = filepath.Join(preprocPath, targetFilePath)
 	}
 
-	properties := ctx.BuildProperties
+	properties := ctx.BuildProperties.Clone()
 	properties[constants.BUILD_PROPERTIES_SOURCE_FILE] = sourceFilePath
 	properties[constants.BUILD_PROPERTIES_PREPROCESSED_FILE_PATH] = targetFilePath
 

src/arduino.cc/builder/includes_finder_with_gcc.go
@@ -45,7 +45,7 @@ type IncludesFinderWithGCC struct {
 }
 
 func (s *IncludesFinderWithGCC) Run(ctx *types.Context) error {
-	buildProperties := ctx.BuildProperties
+	buildProperties := ctx.BuildProperties.Clone()
 	verbose := ctx.Verbose
 	logger := ctx.GetLogger()
 

src/arduino.cc/builder/recipe_runner.go
@@ -50,7 +50,7 @@ func (s *RecipeByPrefixSuffixRunner) Run(ctx *types.Context) error {
 		logger.Fprintln(os.Stdout, constants.LOG_LEVEL_DEBUG, constants.MSG_LOOKING_FOR_RECIPES, s.Prefix, s.Suffix)
 	}
 
-	buildProperties := ctx.BuildProperties
+	buildProperties := ctx.BuildProperties.Clone()
 	verbose := ctx.Verbose
 
 	recipes := findRecipes(buildProperties, s.Prefix, s.Suffix)