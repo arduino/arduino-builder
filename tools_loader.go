137b4560060d8155cd006bb80ec2d2f5cb9be7a8
Martino Facchin <m.facchin@arduino.cc>
2016-08-01 15:13:18 +0200

    allow tools with same name and different vendor

--- tools_loader.go
+++ tools_loader.go
@@ -121,9 +121,9 @@ func collectAllToolsFolders(from string) ([]string, error) {
 	return folders, i18n.WrapError(err)
 }
 
-func toolsSliceContains(tools *[]*types.Tool, name, version string) bool {
+func toolsSliceContains(tools *[]*types.Tool, name, version string, vendor string) bool {
 	for _, tool := range *tools {
-		if name == tool.Name && version == tool.Version {
+		if name == tool.Name && version == tool.Version && vendor == tool.Vendor {
 			return true
 		}
 	}
@@ -148,7 +148,7 @@ func loadToolsFrom(tools *[]*types.Tool, builtinToolsVersionsFilePath string) er
 			toolName := strings.Split(rowParts[0], ".")[1]
 			toolVendor := strings.Split(rowParts[0], ".")[0]
 			toolVersion := rowParts[1]
-			if !toolsSliceContains(tools, toolName, toolVersion) {
+			if !toolsSliceContains(tools, toolName, toolVersion, toolVendor) {
 				*tools = append(*tools, &types.Tool{Name: toolName, Vendor: toolVendor, Version: toolVersion, Folder: folder})
 			}
 		}
@@ -192,7 +192,7 @@ func loadToolsFromFolderStructure(tools *[]*types.Tool, folder string) error {
 				return i18n.WrapError(err)
 			}
 			toolVendor := filepath.Base(strings.Replace(folder, filepath.Base(folder), "", -1))
-			if !toolsSliceContains(tools, toolName.Name(), toolVersion.Name()) {
+			if !toolsSliceContains(tools, toolName.Name(), toolVersion.Name(), toolVendor) {
 				*tools = append(*tools, &types.Tool{Name: toolName.Name(), Vendor: toolVendor, Version: toolVersion.Name(), Folder: toolFolder})
 			}
 		}
