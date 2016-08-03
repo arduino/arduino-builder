d3c36461cf81fbfdb357b039e3098e8d13f83def
Martino Facchin <m.facchin@arduino.cc>
2016-08-03 15:22:39 +0200

    Fix TestLoadSketchFromFolder test on Win

--- src/arduino.cc/builder/test/sketch_loader_test.go
+++ src/arduino.cc/builder/test/sketch_loader_test.go
@@ -124,10 +124,10 @@ func TestLoadSketchFromFolder(t *testing.T) {
 	require.Equal(t, 0, len(sketch.OtherSketchFiles))
 
 	require.Equal(t, 4, len(sketch.AdditionalFiles))
-	require.Contains(t, sketch.AdditionalFiles[0].Name, "sketch_with_subfolders/src/subfolder/other.cpp")
-	require.Contains(t, sketch.AdditionalFiles[1].Name, "sketch_with_subfolders/src/subfolder/other.h")
-	require.Contains(t, sketch.AdditionalFiles[2].Name, "sketch_with_subfolders/subfolder/dont_load_me.cpp")
-	require.Contains(t, sketch.AdditionalFiles[3].Name, "sketch_with_subfolders/subfolder/other.h")
+	require.Contains(t, filepath.ToSlash(sketch.AdditionalFiles[0].Name), "sketch_with_subfolders/src/subfolder/other.cpp")
+	require.Contains(t, filepath.ToSlash(sketch.AdditionalFiles[1].Name), "sketch_with_subfolders/src/subfolder/other.h")
+	require.Contains(t, filepath.ToSlash(sketch.AdditionalFiles[2].Name), "sketch_with_subfolders/subfolder/dont_load_me.cpp")
+	require.Contains(t, filepath.ToSlash(sketch.AdditionalFiles[3].Name), "sketch_with_subfolders/subfolder/other.h")
 }
 
 func TestLoadSketchWithBackup(t *testing.T) {
