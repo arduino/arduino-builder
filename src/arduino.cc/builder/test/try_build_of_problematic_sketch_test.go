251ddaea30fee5809e6862820c471d0dd45ec92a
Martino Facchin <m.facchin@arduino.cc>
2016-11-29 15:02:11 +0100

    Add tests for C linkage proto generation

--- src/arduino.cc/builder/test/try_build_of_problematic_sketch_test.go
+++ src/arduino.cc/builder/test/try_build_of_problematic_sketch_test.go
@@ -205,6 +205,10 @@ func TestTryBuild039(t *testing.T) {
 	tryBuildWithContext(t, ctx, "sketch12", "sketch12.ino")
 }
 
+func TestTryBuild040(t *testing.T) {
+	tryBuild(t, "sketch_with_externC_multiline", "sketch_with_externC_multiline.ino")
+}
+
 func makeDefaultContext(t *testing.T) *types.Context {
 	DownloadCoresAndToolsAndLibraries(t)
 
