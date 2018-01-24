748c70d06261dd74c87a2473f26de707e7f17827
Martino Facchin <m.facchin@arduino.cc>
2018-01-24 16:23:01 +0100

    Revert "[CMAKE] include Arduino.h in main sketch file"
    This reverts commit e7600b54770fe353094090049dd87f6b9fa91032.

--- sketch_source_merger.go
+++ sketch_source_merger.go
@@ -31,10 +31,9 @@
 package builder
 
 import (
-	"regexp"
-
 	"github.com/arduino/arduino-builder/types"
 	"github.com/arduino/arduino-builder/utils"
+	"regexp"
 )
 
 type SketchSourceMerger struct{}
@@ -44,12 +43,12 @@ func (s *SketchSourceMerger) Run(ctx *types.Context) error {
 
 	lineOffset := 0
 	includeSection := ""
-	includeSection += "#line 1 " + utils.QuoteCppString(sketch.MainFile.Name) + "\n"
-	lineOffset++
 	if !sketchIncludesArduinoH(&sketch.MainFile) {
 		includeSection += "#include <Arduino.h>\n"
 		lineOffset++
 	}
+	includeSection += "#line 1 " + utils.QuoteCppString(sketch.MainFile.Name) + "\n"
+	lineOffset++
 	ctx.IncludeSection = includeSection
 
 	source := includeSection
