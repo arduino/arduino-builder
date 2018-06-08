acfd90476fa243f707c94147757522679ce3d585
Cristian Maglie <c.maglie@arduino.cc>
2018-06-08 17:47:43 +0200

    Removed KeysOfMapOfString* method

--- utils/utils.go
+++ utils/utils.go
@@ -52,22 +52,6 @@ import (
 	"golang.org/x/text/unicode/norm"
 )
 
-func KeysOfMapOfStringInterface(input map[string]interface{}) []string {
-	var keys []string
-	for key, _ := range input {
-		keys = append(keys, key)
-	}
-	return keys
-}
-
-func KeysOfMapOfString(input map[string]string) []string {
-	var keys []string
-	for key, _ := range input {
-		keys = append(keys, key)
-	}
-	return keys
-}
-
 func PrettyOSName() string {
 	switch osName := runtime.GOOS; osName {
 	case "darwin":
