90b8a31ae4f26d2c26d2be430b63ce439c9bb5c4
Martino Facchin <m.facchin@arduino.cc>
2018-10-05 15:11:47 +0200

    Ensure that no spurious file is added to AdditionalFiles
    If the build is executed "in place" (inside the sketch folder) intermediate cpp files were added to the AdditionaFiles list.
    This was solved (badly) in https://github.com/arduino/arduino-builder/commit/849faa1f40336d51c9b16726213619e3d01cc19d#diff-b54df391d234f64238253f1d968e2e39R89 .
    
    This patch fixes the issue by not including these files in the list in the first place, so we can avoid removing them later.

--- wipeout_build_path_if_build_options_changed.go
+++ wipeout_build_path_if_build_options_changed.go
@@ -33,7 +33,6 @@ import (
 	"encoding/json"
 	"os"
 	"path/filepath"
-	"strings"
 
 	"github.com/arduino/arduino-builder/builder_utils"
 	"github.com/arduino/arduino-builder/constants"
@@ -85,10 +84,6 @@ func (s *WipeoutBuildPathIfBuildOptionsChanged) Run(ctx *types.Context) error {
 	files, err := gohasissues.ReadDir(buildPath)
 	if err != nil {
 		return i18n.WrapError(err)
-	}
-	// if build path is inside the sketch folder, also wipe ctx.AdditionalFiles
-	if strings.Contains(ctx.BuildPath, filepath.Dir(ctx.SketchLocation)) {
-		ctx.Sketch.AdditionalFiles = ctx.Sketch.AdditionalFiles[:0]
 	}
 	for _, file := range files {
 		os.RemoveAll(filepath.Join(buildPath, file.Name()))
