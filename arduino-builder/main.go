cb100d0e47f68cba82430f376157d860d79c1d83
Martino Facchin <m.facchin@arduino.cc>
2019-03-07 11:01:58 +0100

    Release 1.4.4

--- arduino-builder/main.go
+++ arduino-builder/main.go
@@ -55,7 +55,7 @@ import (
 	"github.com/go-errors/errors"
 )
 
-const VERSION = "1.4.3"
+const VERSION = "1.4.4"
 
 const FLAG_ACTION_COMPILE = "compile"
 const FLAG_ACTION_PREPROCESS = "preprocess"
